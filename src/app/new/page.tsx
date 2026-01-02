bf587eb1955df1ce5e4dfe88d63f5e47237e0879
sachigoyal <sachigoyal27@gmail.com>
2026-01-02 20:57:59 +0600

    fix

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
-import Sidebar1 from "@/components/app-sidebar"
 
 export default function NewNote() {
   return(
