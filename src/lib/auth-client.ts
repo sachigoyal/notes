168a0c0aef46679130b4a5737e1ec44e549e887c
sachigoyal <sachigoyal27@gmail.com>
2026-01-03 00:23:53 +0600

    .

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const { useSession, signIn, signUp, signOut } = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL as string
 })
